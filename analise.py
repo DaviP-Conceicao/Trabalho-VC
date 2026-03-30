e9c2f82e55d946a856802f1940c4a2fd4278c831
DaviP-Conceicao <davipaulino7@hotmail.com>
2026-03-30 18:55:51 -0300

    3.1

---
+++ analise.py
@@ -0,0 +1,33 @@
+import cv2
+import matplotlib.pyplot as plt
+
+caminho = 'imagem.jpg'
+img_colorida = cv2.imread(caminho)
+
+if img_colorida is None:
+    print("Erro")
+else:
+    
+    img_verde = img_colorida[:, :, 1]
+
+    hist = cv2.calcHist([img_verde], [0], None, [256], [0, 256])
+
+
+    plt.figure(" Retinopatia ", figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_verde, cmap='gray')
+    plt.title("Canal Verde (Green Channel)")
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(hist, color='green')
+    plt.title("Histograma de Intensidades")
+    plt.xlabel("Nível de Cinza")
+    plt.ylabel("Quantidade de Pixels")
+    plt.xlim([0, 256])
+
+    plt.tight_layout()
+    plt.show()
+    
+    #3.1
